7a76dc9eb7a0b74ed3e830bc49c4295b7f87b138
Abiola Fajimi <frostactive@gmail.com>
2024-07-10 11:01:29 -0400

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { AppService } from './app.service';
+import { User as UserModel } from '@prisma/client';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Get('user/:id')
+  async getUserById(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id: Number(id) });
+  }
+}
